dfe2147177f958a60600dcd28018ff71ca8c8921
vikvok <v.v.v90@mail.ru>
2017-03-23 11:25:55 +0200

    if

--- gallery/app.js
+++ gallery/app.js
@@ -1,13 +1,6 @@
 angular.module("gallery", [])
 .controller("firstCtrl", function($scope){
 
-	$scope.overlay = false;
-	$scope.imgZoom = false;
-	$scope.gallery = true;
-	$scope.favorites = false;
-	
-	$scope.data = {visible : false};
-
 	$scope.arrImg = [
 	{ name: "foto 1", url: "img/foto01.jpg" },
 	{ name: "foto 2", url: "img/foto02.jpg" },
@@ -21,7 +14,18 @@ angular.module("gallery", [])
 	{ name: "foto 10", url: "img/foto10.jpg" },
 	];
 
-<<<<<<< HEAD
+	$scope.imageIndex = -1;
+	$scope.gallery = true;
+	$scope.favorites = false;
+
+	$scope.openItem = function(idx) {
+		$scope.imageIndex = idx;
+	};
+
+	$scope.closeItem = function() {
+		$scope.imageIndex = -1;
+	};
+
 	$scope.openGallery = function() {
 		$scope.gallery = true;
 		$scope.favorites = false;
@@ -32,39 +36,5 @@ angular.module("gallery", [])
 		$scope.favorites = true;
 	};
 
-	$scope.openItem = function(item) {
-		$scope.overlay = true;
-		$scope.imgZoom = true;
-		$scope.item = item;
-
-=======
-	/*$scope.openItem = function(item) {
-		overlay.style.display = 'block';
-		imgZoom.style.display = 'block';
-		imgZoom.src = item;
->>>>>>> origin/master
-
-	};
-
-	$scope.closeItem = function() {
-<<<<<<< HEAD
-		$scope.overlay = false;
-		$scope.imgZoom = false;
-=======
-		imgZoom.src = '';
-		overlay.style.display = 'none';
-		imgZoom.style.display = 'none';
-	}*/
-
-	$scope.imageIndex = -1;
-
-	$scope.openItem = function(idx) {
-		$scope.imageIndex = idx;
-	};
-
-	$scope.closeItem = function() {
-		$scope.imageIndex = -1;
->>>>>>> origin/master
-	};
 
 });
