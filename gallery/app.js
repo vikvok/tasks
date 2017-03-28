0255817c90e3f12b20bb00d38c847b9a75490a8d
vikvok <v.v.v90@mail.ru>
2017-03-28 18:12:11 +0300

    done

--- gallery/app.js
+++ gallery/app.js
@@ -1,43 +1,37 @@
 angular.module("gallery", [])
 .controller("firstCtrl", function($scope){
 
+	$scope.overlay = false;
+	$scope.favorites = false;
+	
 	$scope.arrImg = [
-	{ name: "foto 1", url: "img/foto01.jpg", favorite: false },
-	{ name: "foto 2", url: "img/foto02.jpg", favorite: false },
-	{ name: "foto 3", url: "img/foto03.jpg", favorite: false },
-	{ name: "foto 4", url: "img/foto04.jpg", favorite: false },
-	{ name: "foto 5", url: "img/foto05.jpg", favorite: false },
-	{ name: "foto 6", url: "img/foto06.jpg", favorite: false },
-	{ name: "foto 7", url: "img/foto07.jpg", favorite: false },
-	{ name: "foto 8", url: "img/foto08.jpg", favorite: false },
-	{ name: "foto 9", url: "img/foto09.jpg", favorite: false },
-	{ name: "foto 10", url: "img/foto10.jpg", favorite: false },
+	{ name: "foto 1", url: "img/foto01.jpg" },
+	{ name: "foto 2", url: "img/foto02.jpg" },
+	{ name: "foto 3", url: "img/foto03.jpg" },
+	{ name: "foto 4", url: "img/foto04.jpg" },
+	{ name: "foto 5", url: "img/foto05.jpg" },
+	{ name: "foto 6", url: "img/foto06.jpg" },
+	{ name: "foto 7", url: "img/foto07.jpg" },
+	{ name: "foto 8", url: "img/foto08.jpg" },
+	{ name: "foto 9", url: "img/foto09.jpg" },
+	{ name: "foto 10", url: "img/foto10.jpg" },
 	];
 
-	$scope.imageIndex = -1;
-	$scope.gallery = true;
-	$scope.favorites = false;
-
-	$scope.openItem = function(idx) {
-		$scope.imageIndex = idx;
-	};
-
-	$scope.closeItem = function() {
-		$scope.imageIndex = -1;
-	};
-
 	$scope.openGallery = function() {
-		$scope.gallery = true;
 		$scope.favorites = false;
 	};
 
 	$scope.openFavorites = function() {
-		$scope.gallery = false;
 		$scope.favorites = true;
 	};
 
-	$scope.change = function(idx) {
-		$scope.arrImg[idx].favorite = !$scope.arrImg[idx].favorite;
+	$scope.openItem = function(item) {
+		$scope.overlay = true;
+		$scope.item = item;
+	};
+
+	$scope.closeItem = function() {
+		$scope.overlay = false;
 	};
 
 });
