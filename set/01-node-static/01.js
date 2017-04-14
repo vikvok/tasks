5ae66fd64e189ed02955dc2695a1362b048edd57
vikvok <v.v.v90@mail.ru>
2017-04-14 16:39:02 +0300

    set

--- set/01-node-static/01.js
+++ set/01-node-static/01.js
@@ -2,14 +2,32 @@
 'use strict';
 
 var stat=require('node-static'),
-	http=require('http'),
-	fileServer=new stat.Server('./public');
+http=require('http'),
+url=require('url'),
+qs=require('querystring'),
+fileServer=new stat.Server('./public');
 
 http.createServer(reqProc).listen(8080);
 console.log('node-static server is running at port 8080');
 
 function reqProc(req, resp) {
-	req.addListener('end', function() {
-		fileServer.serve(req, resp);
-	}).resume();
+
+	var u = req.url;
+	var parsed = url.parse(req.url);
+	var pathName = parsed.pathname;
+	var query =  parsed.query;
+	console.log('pathname = ', pathName);
+	console.log('query = ', parsed.query);
+
+
+switch(pathName) {
+		case '/req1': 
+			resp.writeHead(200,{'content-type':'text'});
+			resp.write('go');
+			resp.end();
+			break;
+		default:
+			fileServer.serve(req, resp);
+			break;
+	}
 }
